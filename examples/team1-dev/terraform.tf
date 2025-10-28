f693ac4d3349deff259b98b81aab589d38a3ecc6
mattspahr-hashi <matt.spahr@hashicorp.com>
2025-10-28 13:42:11 -0500

    Update terraform.tf

--- examples/team1-dev/terraform.tf
+++ examples/team1-dev/terraform.tf
@@ -1,12 +1,8 @@
 provider "vault" {
-  address = var.vault_address
+  address = "https://sample-cluster-public-vault-db854a77.7f7cc99c.z1.hashicorp.cloud:8200"
   token   = var.vault_token
 }
 
 variable "vault_token" {
-  type = string
-}
-
-variable "vault_address" {
   type = string
 }
