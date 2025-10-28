931b6262c1ef43c771c11e6ff80d4786953d7f26
Matt Spahr <matt.spahr@matt.spahr-M4MGYYK3WC>
2025-10-28 11:45:26 -0500

    added policy as code

--- examples/team1-dev/team1-dev.tf
+++ examples/team1-dev/team1-dev.tf
@@ -1,14 +1,16 @@
-module "team1_secrets" {
-  source = "../../modules/team-secrets-engine"
-
-  team_name   = "team"
+module "vault-kv" {
+  source  = "app.terraform.io/mattspahr-sandbox/vault-kv/vault"
+  version = "1.0.1"
+  # insert required variables here
+  team_name   = "payments"
+  service_name = "payment-service"
   environment = "dev"
 }
 
 output "secrets_engine_path" {
-  value = module.team1_secrets.secrets_engine_path
+  value = module.vault-kv.secrets_engine_path
 }
 
 output "policy_name" {
-  value = module.team1_secrets.policy_name
+  value = module.vault-kv.policy_name
 }
